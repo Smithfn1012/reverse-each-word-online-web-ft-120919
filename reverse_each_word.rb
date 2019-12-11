22c775f132908b4ca1304ada2d3a735dbfb19872
Felicia Smith <smithfn1012@gmail.com>
2019-12-11 18:08:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
 def reverse_each_word(string)
   string = string.split(" ")
-  newString = string.collect{|reverse_string| reverse_string.reverse
+  newString = string.collect{|reverse_string| reverse_string.reverse}
   
